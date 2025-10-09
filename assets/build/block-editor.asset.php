86667a59edeacf6419c87a15d0067d053e96cc0f
Raphaël Droz <raphael.droz@gmail.com>
2025-10-09 16:39:02 -0300

    added build files

---
+++ assets/build/block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '3cf5ff9e47946fdf7fbd');
---
+++ assets/build/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '82b9c8cfc7bdbf3551ce');
